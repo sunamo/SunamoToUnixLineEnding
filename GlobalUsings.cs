c40310aa8fee972585ea6932333ddad10faeffcb
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-15 16:09:36 +0200

    Before commented values classes

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,1 +1,12 @@
 global using System.Collections.Generic;
+global using SunamoToUnixLineEnding._sunamo.SunamoExceptions;
+global using SunamoToUnixLineEnding._sunamo.SunamoValues;
+global using System;
+global using System.Linq;
+global using System.Text;
+global using System.Collections;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Reflection;
+global using System.Net;
+global using SunamoToUnixLineEnding;
+global using System.Diagnostics;
---
+++ _sunamo/SunamoExceptions/TranslateAble.cs
@@ -0,0 +1,8 @@
+namespace SunamoToUnixLineEnding._sunamo.SunamoExceptions;
+internal class TranslateAble
+{
+    internal static string i18n(string v)
+    {
+        return v;
+    }
+}

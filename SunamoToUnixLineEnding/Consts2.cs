0e3211938301fb046965c2473104c39e74a439dd
smutekutek <smutekutek@gmail.com>
2026-06-24 23:48:52 +0200

    Ahoj! Jak ti mohu pomoci?

--- SunamoToUnixLineEnding/Consts2.cs
+++ SunamoToUnixLineEnding/Consts2.cs
@@ -1,17 +1,8 @@
 namespace SunamoToUnixLineEnding;
 
-/// <summary>
-/// Constants for line ending characters used in cross-platform text processing.
-/// </summary>
 public class Consts2
 {
-    /// <summary>
-    /// Unix line ending (line feed only).
-    /// </summary>
     public const string Newline = "\n";
 
-    /// <summary>
-    /// Windows line ending (carriage return followed by line feed).
-    /// </summary>
     public const string CarriageReturnNewline = "\r\n";
 }
--- SunamoToUnixLineEnding/StringToUnixLineEndingExtensions.cs
+++ SunamoToUnixLineEnding/StringToUnixLineEndingExtensions.cs
@@ -1,15 +1,7 @@
 namespace SunamoToUnixLineEnding;
 
-/// <summary>
-/// Extension methods for converting line endings to Unix format in strings.
-/// </summary>
 public static class StringToUnixLineEndingExtensions
 {
-    /// <summary>
-    /// Converts all line endings in the string to Unix line endings (LF only).
-    /// </summary>
-    /// <param name="text">The string whose line endings will be converted.</param>
-    /// <returns>The string with all line endings replaced by Unix line feed characters.</returns>
     public static string ToUnixLineEnding(this string text)
     {
         return text.ReplaceLineEndings(Consts2.Newline);

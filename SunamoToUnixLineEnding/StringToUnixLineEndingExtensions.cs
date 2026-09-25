8235d0e9cec912f558de45391a73c4a517154f4b
Radek Jančík <radek.jancik@sunamo.cz>
2026-09-25 13:17:43 +0200

    Ahoj! Jak ti mohu pomoci? (#2)
    Co-authored-by: smutekutek <smutekutek@gmail.com>

--- SunamoToUnixLineEnding/StringToUnixLineEndingExtensions.cs
+++ SunamoToUnixLineEnding/StringToUnixLineEndingExtensions.cs
@@ -1,15 +1,7 @@
 namespace SunamoToUnixLineEnding;
 
-/// <summary>
-/// Extension methods for converting line endings to Unix format in strings.
-/// </summary>
 public static class StringToUnixLineEndingExtensions
 {
-    /// <summary>
-    /// Converts all line endings in the string to Unix line endings (LF only).
-    /// </summary>
-    /// <param name="text">The string whose line endings will be converted.</param>
-    /// <returns>The string with all line endings replaced by Unix line feed characters.</returns>
     public static string ToUnixLineEnding(this string text)
     {
         return text.Replace(Consts2.CarriageReturnNewline, Consts2.Newline).Replace("\r", Consts2.Newline);
